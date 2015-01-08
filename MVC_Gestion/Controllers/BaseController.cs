25ce6b6a8bd412dc1e476f0d521b6b541fe22705
Williams <w.contreras.f@gmail.com>
2015-01-08 00:47:57 -0300

    Pendiente Revision
    revisar el nullpointerexception al recuperar el username

--- MVC_Gestion/Controllers/BaseController.cs
+++ MVC_Gestion/Controllers/BaseController.cs
@@ -16,25 +16,42 @@ namespace MVC_Gestion.Controllers
 
         private IList<MenuModel> BuildMenu()
         {
-            IList<MenuModel> mmList = new List<MenuModel>(){
+            String usrNick = HttpContext.User.Identity.Name;
+            Models.Negocio.MenuNegocio mn = new Models.Negocio.MenuNegocio();
+            List<MenuModel> ls = new List<MenuModel>();
 
-                // Parent
-                new MenuModel(){ menu_id=1, menu_nombre="Inicio", menu_grupo=1, menu_funcmain=1, menu_funcsec=2, menu_url="#"} ,
-                new MenuModel(){ menu_id=2, menu_nombre="Mejora", menu_grupo=2, menu_funcmain=1, menu_funcsec=2, menu_url="#"} ,
-                new MenuModel(){ menu_id=3, menu_nombre="Seguimiento", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#"} ,
-                new MenuModel(){ menu_id=4, menu_nombre="Salir", menu_grupo=4, menu_funcmain=1, menu_funcsec=2, menu_url="#"} ,
-                // Children
+            if (ModelState.IsValid)
+            {
+                if (Request.IsAuthenticated)
+                {
+                    if (HttpContext.User.Identity.Name.Equals(usrNick))
+                    {
+                        ls.AddRange(mn.MenuUsuario(mn.usrPerfil(usrNick)));
+                        ls.AddRange(mn.SubMenuUsuario(mn.usrPerfil(usrNick)));
+                    }
+                }
+            }
 
-                new MenuModel() { menu_id=5, menu_nombre="Nueva Mejora", menu_grupo=2, menu_funcmain=1, menu_funcsec=2, menu_url="#" },
-                new MenuModel() { menu_id=6, menu_nombre="Ver Mejora", menu_grupo=2, menu_funcmain=1, menu_funcsec=2, menu_url="#" },
+            return ls;
+            //IList<MenuModel> mmList = new List<MenuModel>(){
 
-                new MenuModel() { menu_id=7, menu_nombre="Ver Mejora", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#"},
-                new MenuModel() { menu_id=8, menu_nombre="Nuevo Seguimiento", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#" },
-                new MenuModel() { menu_id=9, menu_nombre="Cerrar Seguimiento", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#" },
+            //    // Parent
+            //    new MenuModel(){ menu_id=1, menu_nombre="Inicio", menu_grupo=1, menu_funcmain=1, menu_funcsec=2, menu_url="#"} ,
+            //    new MenuModel(){ menu_id=2, menu_nombre="Mejora", menu_grupo=2, menu_funcmain=1, menu_funcsec=2, menu_url="#"} ,
+            //    new MenuModel(){ menu_id=3, menu_nombre="Seguimiento", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#"} ,
+            //    new MenuModel(){ menu_id=4, menu_nombre="Salir", menu_grupo=4, menu_funcmain=1, menu_funcsec=2, menu_url="#"} ,
+            //    // Children
 
-            };
+            //    new MenuModel() { menu_id=5, menu_nombre="Nueva Mejora", menu_grupo=2, menu_funcmain=1, menu_funcsec=2, menu_url="#" },
+            //    new MenuModel() { menu_id=6, menu_nombre="Ver Mejora", menu_grupo=2, menu_funcmain=1, menu_funcsec=2, menu_url="#" },
 
-            return mmList;
+            //    new MenuModel() { menu_id=7, menu_nombre="Ver Mejora", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#"},
+            //    new MenuModel() { menu_id=8, menu_nombre="Nuevo Seguimiento", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#" },
+            //    new MenuModel() { menu_id=9, menu_nombre="Cerrar Seguimiento", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#" },
+
+            //};
+
+            //return mmList;
         }
     }
 }
