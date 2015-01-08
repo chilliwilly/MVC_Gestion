f8bc59961ffda580df409d56fc8fd315f9e9ea41
Williams <w.contreras.f@gmail.com>
2015-01-08 00:48:34 -0300

    Revert "Revert "Pendiente Revision""
    This reverts commit 3f1bc72531d3814e7a5613e8454a09563c44ac43.

--- MVC_Gestion/Models/ModelGestion.Context.cs
+++ MVC_Gestion/Models/ModelGestion.Context.cs
@@ -28,5 +28,6 @@ namespace MVC_Gestion.Models
         public DbSet<TBLUSUARIO> TBLUSUARIO { get; set; }
         public DbSet<TBLUSUARIOROL> TBLUSUARIOROL { get; set; }
         public DbSet<TBLROL> TBLROL { get; set; }
+        public DbSet<TBLMENU> TBLMENU { get; set; }
     }
 }
