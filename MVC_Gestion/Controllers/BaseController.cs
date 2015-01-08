195efb36c23b32ec01fa1038aba44983e68f714b
Williams <w.contreras.f@gmail.com>
2015-01-08 00:25:01 -0300

    Modificacion Menu
    se intenta hacer correr el menu dinamico tanto con roles como generado a
    partir de clases y funciones

---
+++ MVC_Gestion/Controllers/BaseController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Gestion.Models;
+
+namespace MVC_Gestion.Controllers
+{
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+            ViewBag.Menu = BuildMenu();
+        }
+
+        private IList<MenuModel> BuildMenu()
+        {
+            IList<MenuModel> mmList = new List<MenuModel>(){
+
+                // Parent
+                new MenuModel(){ menu_id=1, menu_nombre="Inicio", menu_grupo=1, menu_funcmain=1, menu_funcsec=2, menu_url="#"} ,
+                new MenuModel(){ menu_id=2, menu_nombre="Mejora", menu_grupo=2, menu_funcmain=1, menu_funcsec=2, menu_url="#"} ,
+                new MenuModel(){ menu_id=3, menu_nombre="Seguimiento", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#"} ,
+                new MenuModel(){ menu_id=4, menu_nombre="Salir", menu_grupo=4, menu_funcmain=1, menu_funcsec=2, menu_url="#"} ,
+                // Children
+
+                new MenuModel() { menu_id=5, menu_nombre="Nueva Mejora", menu_grupo=2, menu_funcmain=1, menu_funcsec=2, menu_url="#" },
+                new MenuModel() { menu_id=6, menu_nombre="Ver Mejora", menu_grupo=2, menu_funcmain=1, menu_funcsec=2, menu_url="#" },
+
+                new MenuModel() { menu_id=7, menu_nombre="Ver Mejora", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#"},
+                new MenuModel() { menu_id=8, menu_nombre="Nuevo Seguimiento", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#" },
+                new MenuModel() { menu_id=9, menu_nombre="Cerrar Seguimiento", menu_grupo=3, menu_funcmain=1, menu_funcsec=0, menu_url="#" },
+
+            };
+
+            return mmList;
+        }
+    }
+}
